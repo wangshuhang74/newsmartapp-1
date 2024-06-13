e76f79a8efcfe4dfde5219cc36d59df07e56d8f0
wsh <3112475244@qq.com>
2024-06-13 17:26:29 +0800

    测试pinia

--- main.js
+++ main.js
@@ -2,10 +2,14 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
 import * as Pinia from 'pinia' // 创建Pinia实例
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+// import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { persist } from "./store/persist";
+const pinia = Pinia.createPinia();
+// pinia.use(piniaPluginPersistedstate)
+pinia.use(persist);
 export function createApp() {
   const app = createSSRApp(App)
-  app.use(Pinia.createPinia().use(piniaPluginPersistedstate))
+  app.use(pinia);
   return {
     app,
     Pinia
